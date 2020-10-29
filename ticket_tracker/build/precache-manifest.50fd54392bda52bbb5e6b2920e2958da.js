5402d44ae4889b69186fd12094655fee5d4d281a
Austen Cote <austencote@Austens-MBP.home>
2020-10-28 17:21:40 -0700

    added build -ARC

---
+++ ticket_tracker/build/precache-manifest.50fd54392bda52bbb5e6b2920e2958da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af44d084acae16fd4546d460d483e9d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "956c210dc5753eff6d99",
+    "url": "/static/css/main.fbec074b.chunk.css"
+  },
+  {
+    "revision": "aa187053d6603b46225b",
+    "url": "/static/js/2.2059e274.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.2059e274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "956c210dc5753eff6d99",
+    "url": "/static/js/main.1ea63389.chunk.js"
+  },
+  {
+    "revision": "ea7cee67bfe696615ef9",
+    "url": "/static/js/runtime-main.4c6c3c1c.js"
+  }
+]);
